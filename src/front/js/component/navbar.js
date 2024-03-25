3cf8a270629f2706aa4b531c3a00938825eb438a
Leonardo Rodriguez <106454639+Leonard392@users.noreply.github.com>
2024-03-25 20:55:06 +0000

    working on styles login page

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light">
+		<nav className="navbar bg-light">
 			<div className="container-fluid">
 				<Link className="brand-link" to="/">
 					<p className="brand">Ninja Station</p>
--- src/front/js/pages/login.js
+++ src/front/js/pages/login.js
@@ -1,21 +1,27 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 import { Link } from "react-router-dom";
+import { Navbar } from "../component/navbar";
 import ProfilePic from "../../img/profile.png";
 import "../../styles/login.css";
 
 export const Login = () => {
 	return (
+		<div>
+		<Navbar/>
+		<div className="login-body">
 		<div className="login">
 			<img src={ProfilePic} />
 			<h3>Welcome Back!</h3>
-			<h2>Leo Rod</h2>	
+			<h2>GaMeR</h2>	
 			<form className="login-form">
+				<input type="text" placeholder="Enter your user"/>
 				<input type="password" placeholder="Enter your passcode"/>
 				<button type="submit">LOGIN</button>
 				<a href="#">Forget your passcode?</a>
 			</form>
-
+		</div>
+		</div>
 		</div>
 	);
 };
