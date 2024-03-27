17b1821df9abea7a750b4960154ad176d4478abb
Leonardo Rodriguez <106454639+Leonard392@users.noreply.github.com>
2024-03-27 20:17:08 +0000

    e

--- src/front/js/pages/main.js
+++ src/front/js/pages/main.js
@@ -1,6 +1,8 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 import { Link } from "react-router-dom";
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 import { NavbarSignedIn } from "../component/navbarSignedIn.jsx";
 import "../../styles/navbarSignedIn.css";
 
@@ -8,7 +10,17 @@ export const Main = () => {
 	return (
 		<div>
 		    <NavbarSignedIn/>
-            <h1>Hello I am Main Page</h1>
+            <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text className="card-text">
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
 		</div>
 	);
 };
