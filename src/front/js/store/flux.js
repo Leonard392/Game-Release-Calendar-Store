de63b81d0cf2fadadaf7502a2dd5a6d917b59cbb
Leonardo Rodriguez <106454639+Leonard392@users.noreply.github.com>
2024-03-30 16:05:47 +0000

    im

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -14,13 +14,29 @@ const getState = ({ getStore, getActions, setStore }) => {
 				try{
 					// Function to fetch games for a given year and month from Api
 					
-					
+					year = "2024";
+					month="03";
 					// fetching data from the backend
 					const resp = await fetch("https://api.rawg.io/api/games?dates=" + year  + "-" + (month + 1) + "-01," + year + "-" + (month + 1) + "-30&key=" + KEY_API)
-					const data = await resp.json()
-					setStore({ getGamesList: data.results})
+					.then(response => response.json())
+					.then(data => {
+						const games = data.results.map(item =>
+							fetch(item.url)
+							.then(response => response.json())
+							.then(characterDetails => ({
+								name: characterDetails.result.properties.name
+							}))
+						);
+						
+						Promise.all(games)
+						.then(updatedCharactersList => {
+						setStore({ getGamesList: updatedCharactersList });
+						})
+						.catch(err => console.error(err));
+					});
+					
 					// don't forget to return something, that is how the async resolves
-					return data;
+
 				}catch(error){
 					console.log("Error loading message from backend", error)
 				}
