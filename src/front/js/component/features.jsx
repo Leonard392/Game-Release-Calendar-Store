f48ed2b925812cd7dfecda2e91ebf1207cad06c9
Leonardo Rodriguez <106454639+Leonard392@users.noreply.github.com>
2024-04-18 22:55:21 +0000

    features styles done

--- src/front/js/component/features.jsx
+++ src/front/js/component/features.jsx
@@ -1,31 +1,31 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import "../../styles/features.css"
 
 export const Features = () => {
 	return (
 		<div  className="features row">
             <h1 className="features-title">Features</h1>
-            <p className="features-description">Explore our online video game store, where every gamer finds their perfect match.</p>
+            <h4 className="general-description">Explore our online video game store, where every gamer finds their perfect match.</h4>
             <div className="cards row">
             <div className="feature-card col-3">
-                <h3 className="feat-name">User Friendly Design</h3>
-                <i class="fa-solid fa-user-group"></i> 
-                <p className="feat-description">Some Explaning Text</p>
+                <h3 className="feat-name">Friendly Design</h3>
+                <i class="feat-icon fa-solid fa-user-group fa-5x"></i> 
+                <p className="feat-description">This page ensures effortless navigation with engaging visuals. Interactive features personalize the experience, enhancing enjoyment for all players.</p>
             </div>
             <div className="feature-card col-3">
                 <h3 className="feat-name">WishList</h3>
-                <i class="fa-solid fa-heart"></i> 
-                <p className="feat-description">Some Explaning Text</p>
+                <i class="feat-icon fa-solid fa-heart fa-5x"></i> 
+                <p className="feat-description">The gaming page's wishlist lets players easily save and track desired games, adding convenience and excitement to their gaming journey.</p>
             </div>
             <div className="feature-card col-3">
                 <h3 className="feat-name">Games Reviews</h3>
-                <i class="fa-solid fa-comment"></i> 
-                <p className="feat-description">Some Explaning Text</p>
+                <i class="feat-icon fa-solid fa-comment fa-5x"></i> 
+                <p className="feat-description">We offer insights into gameplay, graphics, and enjoyment, guiding players to discover new favorites and make informed choices.</p>
             </div>
             <div className="feature-card col-3">
                 <h3 className="feat-name">Multiple Platforms</h3>
-                <i class="fa-brands fa-playstation"></i> 
-                <p className="feat-description">Some Explaning Text</p>
+                <i class="feat-icon fa-brands fa-playstation fa-5x"></i> 
+                <p className="feat-description">A gaming page supporting multiple platforms ensures players can enjoy their favorite titles seamlessly across consoles, PCs, and mobile devices.</p>
             </div>
             </div>
         </div>
