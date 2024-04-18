9c81e900b9130221f833222eb2511020e90bf0e1
Leonardo Rodriguez <106454639+Leonard392@users.noreply.github.com>
2024-04-18 16:02:00 +0000

    finishing navbar

--- src/front/js/component/features.jsx
+++ src/front/js/component/features.jsx
@@ -9,30 +9,22 @@ export const Features = () => {
             <div className="cards row">
             <div className="feature-card col-3">
                 <h3 className="feat-name">User Friendly Design</h3>
-                <svg className="feat-icon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-square-fill" viewBox="0 0 16 16">
-                <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zm10.03 4.97a.75.75 0 0 1 .011 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.75.75 0 0 1 1.08-.022z"/>
-                </svg>  
+                <i class="fa-solid fa-user-group"></i> 
                 <p className="feat-description">Some Explaning Text</p>
             </div>
             <div className="feature-card col-3">
                 <h3 className="feat-name">WishList</h3>
-                <svg className="feat-icon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">
-                <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
-                </svg>  
+                <i class="fa-solid fa-heart"></i> 
                 <p className="feat-description">Some Explaning Text</p>
             </div>
             <div className="feature-card col-3">
                 <h3 className="feat-name">Games Reviews</h3>
-                <svg className="feat-icon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chat-left-dots-fill" viewBox="0 0 16 16">
-                <path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H4.414a1 1 0 0 0-.707.293L.854 15.146A.5.5 0 0 1 0 14.793zm5 4a1 1 0 1 0-2 0 1 1 0 0 0 2 0m4 0a1 1 0 1 0-2 0 1 1 0 0 0 2 0m3 1a1 1 0 1 0 0-2 1 1 0 0 0 0 2"/>
-                </svg>  
+                <i class="fa-solid fa-comment"></i> 
                 <p className="feat-description">Some Explaning Text</p>
             </div>
             <div className="feature-card col-3">
                 <h3 className="feat-name">Multiple Platforms</h3>
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-playstation" viewBox="0 0 16 16">
-                <path d="M15.858 11.451c-.313.395-1.079.676-1.079.676l-5.696 2.046v-1.509l4.192-1.493c.476-.17.549-.412.162-.538-.386-.127-1.085-.09-1.56.08l-2.794.984v-1.566l.161-.054s.807-.286 1.942-.412c1.135-.125 2.525.017 3.616.43 1.23.39 1.368.962 1.056 1.356M9.625 8.883v-3.86c0-.453-.083-.87-.508-.988-.326-.105-.528.198-.528.65v9.664l-2.606-.827V2c1.108.206 2.722.692 3.59.985 2.207.757 2.955 1.7 2.955 3.825 0 2.071-1.278 2.856-2.903 2.072Zm-8.424 3.625C-.061 12.15-.271 11.41.304 10.984c.532-.394 1.436-.69 1.436-.69l3.737-1.33v1.515l-2.69.963c-.474.17-.547.411-.161.538.386.126 1.085.09 1.56-.08l1.29-.469v1.356l-.257.043a8.45 8.45 0 0 1-4.018-.323Z"/>
-                </svg>  
+                <i class="fa-brands fa-playstation"></i> 
                 <p className="feat-description">Some Explaning Text</p>
             </div>
             </div>
--- src/front/js/component/navbar.jsx
+++ src/front/js/component/navbar.jsx
@@ -1,130 +1,89 @@
-import React from "react";
+import React, { useState} from "react";
 import { Link } from "react-router-dom";
 import "../../styles/navbar.css"
 
 export const Navbar = () => {
-	const dropDownMenu = document.querySelector(".Dropdown_menu");
-	const toggleBtnIcon = document.querySelector(".toggle_btn ");
+	const [isOpen, setIsOpen] = useState(false);
 
+    const handleClick = () => {
+        setIsOpen(!isOpen);
+    };
 
-	const handleClick = () => {
-		dropDownMenu.clasList.toggle("open");
-		const isOpen = dropDownMenu.classList.contains("open");
+    return (
+        <header>
+            <div className="navBar">
+                <div className="logo">
+                    <Link to="/" className="link-logo">
+                        <h1>Ninja Station</h1>
+                    </Link>
+                </div>
+                <ul className={`links ${isOpen ? "open" : ""}`}>
+                    <li>
+                        <Link to="/platforms" className="naV-link">
+                            <h4>PLATFORMS</h4>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/creators" className="naV-link">
+                            <h4>CREATORS</h4>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/stores" className="naV-link">
+                            <h4>STORES</h4>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/genres/action" className="naV-link">
+                            <h4>GAMES</h4>
+                        </Link>
+                    </li>
+                </ul>
+                <Link to="/signup">
+                    <button className="action_btn">Get Started</button>
+                </Link>
+                <Link to="/login">
+                    <button className="action_btn">Log In</button>
+                </Link>
+                <div className="toggle_btn" onClick={handleClick}>
+                    <i className={`fa-solid ${isOpen ? "fa-xmark" : "fa-bars"}`}></i>
+                </div>
+            </div>
 
-
-	}
-
-	return (
-		<header>
-		<div className="navBar">
-			<div className="logo">
-				<Link to="/" className="link-logo">
-					<h1>Ninja Station</h1>
-				</Link>
-			</div>
-			<ul className="links">
-				<li>
-					<Link to="/platforms" className="naV-link">
-						<h4>PLATFORMS</h4>
-					</Link>
-				</li>
-				<li>
-					<Link to="/creators" className="naV-link">
-						<h4>CREATORS</h4>
-					</Link>
-				</li>
-				<li>
-					<Link to="/stores" className="naV-link">
-						<h4>STORES</h4>
-					</Link>
-				</li>
-				<li>
-					<Link to="/genres/action" className="naV-link">
-						<h4>GAMES</h4>
-					</Link>
-				</li>
-			</ul>
-			<Link to="/signup">
-				<button className="action_btn">Get Started</button>
-			</Link>
-			<Link to="/login">
-				<button className="action_btn">Log In</button>
-			</Link>
-			<div className="toggle_btn" onClick={handleClick}>
-				<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-filter" viewBox="0 0 16 16">
-  					<path d="M6 10.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5m-2-3a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5m-2-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5"/>
-				</svg>
-			</div>
-		</div>
-
-		<div className="Dropdown_menu open">
-		<li>
-			<Link to="/platforms" className="naV-link">
-				<h4>PLATFORMS</h4>
-			</Link>
-		</li>
-		<li>
-			<Link to="/creators" className="naV-link">
-				<h4>CREATORS</h4>
-			</Link>
-		</li>
-		<li>
-			<Link to="/stores" className="naV-link">
-				<h4>STORES</h4>
-			</Link>
-		</li>
-		 <li>
-			<Link to="/genres/action" className="naV-link">
-				<h4>GAMES</h4>
-			</Link>
-		</li>
-		<li>
-		<Link to="/signup" className="naV-btn">
-			<button className="action_btn">Get Started</button>
-		</Link>
-		</li>
-		<li>
-		<Link to="/login" className="naV-btn">
-			<button className="action_btn">Log In</button>
-		</Link>
-		</li>
-		</div>
-		</header>
-	);
+            <div className={`Dropdown_menu ${isOpen ? "open" : ""}`}>
+                <ul>
+                    <li>
+                        <Link to="/platforms" className="naV-link">
+                            <h4>PLATFORMS</h4>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/creators" className="naV-link">
+                            <h4>CREATORS</h4>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/stores" className="naV-link">
+                            <h4>STORES</h4>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/genres/action" className="naV-link">
+                            <h4>GAMES</h4>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/signup" className="naV-btn">
+                            <button className="action_btn">Get Started</button>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/login" className="naV-btn">
+                            <button className="action_btn">Log In</button>
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+        </header>
+    );
 };
-
-
-
-
-<nav className="navbar bg-light">
-			<div className="container-fluid">
-				<Link className="brand-link" to="/">
-					<h1 className="brand">Ninja Station</h1>
-				</Link>
-
-				<div className="center-links">
-					<Link className="center-link" to="/platforms">
-						<h4>PLATFORMS</h4>
-					</Link>
-					<Link className="center-link" to="/creators">
-						<h4>CREATORS</h4>
-					</Link>
-					<Link className="center-link" to="/stores">
-						<h4>STORES</h4>
-					</Link>
-					<Link className="center-link" to="/genres/action">
-						<h4>GAMES</h4>
-					</Link>
-				</div>
-
-
-				<div className="ml-auto">
-					<Link className="signup-link" to="/signup">
-						<button className="signup-btn">Get Started</button>
-					</Link>
-					<Link className="signup-link" to="/login">
-						<button className="login-btn">Log In</button>
-					</Link>
-				</div>
-			</div>
-		</nav>
