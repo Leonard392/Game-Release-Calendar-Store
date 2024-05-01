4541148e634c8b4485367bcf610739d813535158
Leonardo Rodriguez <106454639+Leonard392@users.noreply.github.com>
2024-05-01 04:52:13 +0000

    platforms details styles done

--- src/front/js/component/details/platformDetails.jsx
+++ src/front/js/component/details/platformDetails.jsx
@@ -1,43 +1,38 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import "../../../styles/details.css";
 
-
-export const PlatformDetails = ({ gameId }) => {
-    const params = useParams()
+export const PlatformDetails = () => {
+    const params = useParams();
     const KEY_API = "35e73496eb2c49af83d0f3f7074b875e";
     const [platformDetails, setPlatformDetails] = useState(null);
 
-    let url = 'https://api.rawg.io/api/platforms/' + params.id + "?key=" + KEY_API; 
+    let url = `https://api.rawg.io/api/platforms/${params.id}?key=${KEY_API}`;
 
     useEffect(() => {
         fetch(url)
             .then(response => response.json())
             .then(data => setPlatformDetails(data))
-            .catch(err => err)
+            .catch(err => console.error(err));
     }, []);
 
-    const backgroundStyle = {
-        backgroundImage: platformDetails ? `url(${platformDetails.image_background})` : 'none',
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        color: 'white',
-        textAlign: 'center',
-    };
-
     return (
-        <div style={backgroundStyle}>
+        <div className="details">
             {platformDetails ? (
-                <div>
-                    <h2>{platformDetails.name}</h2>
-                    <p>{platformDetails.description}</p>
-                    {/* Render more details as needed */}
-                </div>
+                <>
+                    <div className="details-left">
+                        {/* Imagen del creador */}
+                        <img src={platformDetails.image_background} alt={platformDetails.name} className="details-img" />
+                    </div>
+                    <div className="details-right">
+                        {/* Detalles de la plataforma */}
+                        <h1><strong>PLATFORM</strong> DETAILS</h1>
+                        <h2>{platformDetails.name}</h2>
+                        <p><strong>Games Count:</strong> {platformDetails.games_count}</p>
+                    </div>
+                </>
             ) : (
-                <p>Loading game details...</p>
+                <p>Loading platform details...</p>
             )}
         </div>
     );
